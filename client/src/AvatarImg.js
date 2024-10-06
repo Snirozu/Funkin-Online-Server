5bd17cefdbbed30901ff4053f5f77217b41e99dc
Snirozu <72814880+Snirozu@users.noreply.github.com>
2024-10-06 14:45:20 +0200

    and this

--- client/src/AvatarImg.js
+++ client/src/AvatarImg.js
@@ -1,6 +1,7 @@
 function AvatarImg(props) {
     var onFailAvatar = (image) => {
-        image.currentTarget.src = 'https://kickstarter.funkin.me/static/assets/img/stickers/bf' + (Math.round(Math.random() + 1)) + '.png';
+        image.currentTarget.src = "https://raw.githubusercontent.com/FunkinCrew/funkin.assets/a6786b4f37e352356dec86576442a169b01f4d43/shared/images/transitionSwag/stickers-set-1/bfSticker" + (Math.round(Math.random() + 1)) + ".png";
+        //image.currentTarget.src = 'https://kickstarter.funkin.me/static/assets/img/stickers/bf' + (Math.round(Math.random() + 1)) + '.png';
     };
 
     return (
