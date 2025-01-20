a782d5a1fe6918e01ea3dd4c9aba725e41c8cdb0
Til Tjardes <til.tjardes@gmail.com>
2025-01-20 15:00:40 -0700

    fix somethin

--- src/rooms/schema/RoomState.ts
+++ src/rooms/schema/RoomState.ts
@@ -22,7 +22,7 @@ export class RoomState extends Schema {
   @type("number") health: number = 0.0;
   @type({ map: "string" }) gameplaySettings = new MapSchema<string>();
   @type("boolean") permitModifiers: boolean = false;
-  @type("boolean") disableSkins: boolean = false;
   @type("boolean") hideGF: boolean = false;
+  @type("boolean") disableSkins: boolean = false;
   @type("number") winCondition: number = 0;
 }
