8c57e9e6d36dca746cda9b3e6d8cf49d32961ad4
Snirozu <72814880+Snirozu@users.noreply.github.com>
2025-11-08 01:42:36 +0100

    thanks googol

--- src/network/email.ts
+++ src/network/email.ts
@@ -1,24 +1,27 @@
 import nodemailer from 'nodemailer';
 import * as crypto from "crypto";
 
-export const transMail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.GMAIL_ID,
-        pass: process.env.GMAIL_PASSWORD
-    }
-});
+// export const transMail = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.GMAIL_ID,
+//         pass: process.env.GMAIL_PASSWORD
+//     }
+// });
 
 export const emailCodes: Map<string, string> = new Map<string, string>();
 export const emailCodeTimers: Map<string, NodeJS.Timeout> = new Map<string, NodeJS.Timeout>();
 
 export async function sendCodeMail(email: string, code: string) {
-    await transMail.sendMail({
-        from: 'Psych Online',
-        to: email,
-        subject: code + ' is your Verification Code',
-        html: '<h3>Your verification code is:<h3><h1>' + code + '</h1>'
-    }
+    emailCodes.delete(email);
+    throw { error_message: "Email functionalities are temporarily disabled!" }
+
+    // await transMail.sendMail({
+    //     from: 'Psych Online',
+    //     to: email,
+    //     subject: code + ' is your Verification Code',
+    //     html: '<h3>Your verification code is:<h3><h1>' + code + '</h1>'
+    // }
         // ,    (error, info) => {
         //         if (res)
         //             if (error)
@@ -26,7 +29,7 @@ export async function sendCodeMail(email: string, code: string) {
         //             else
         //                 res.sendStatus(200);
         //     }
-    );
+    // );
 }
 
 export function tempSetCode(email: string, code: string) {
