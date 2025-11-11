09a24dede6ae6a6d6673a6fd81c7de325e12ff04
Snirozu <72814880+Snirozu@users.noreply.github.com>
2025-11-11 03:02:24 +0100

    www

--- src/network/email.ts
+++ src/network/email.ts
@@ -18,20 +18,25 @@ export const emailCodes: Map<string, string> = new Map<string, string>();
 export const emailCodeTimers: Map<string, NodeJS.Timeout> = new Map<string, NodeJS.Timeout>();
 
 export async function sendCodeMail(email: string, code: string) {
-    await transMail.sendMail({
-        from: 'Psych Online <' + process.env.SMTP_MAIL + '>',
-        to: email,
-        subject: code + ' is your Verification Code',
-        html: '<h3>Your verification code is:<h3><h1>' + code + '</h1>',
+    try {
+        await transMail.sendMail({
+            from: 'Psych Online <' + process.env.SMTP_MAIL + '>',
+            to: email,
+            subject: code + ' is your Verification Code',
+            html: '<h3>Your verification code is:<h3><h1>' + code + '</h1>',
+        }
+            // ,    (error, info) => {
+            //         if (res)
+            //             if (error)
+            //                 res.sendStatus(500);
+            //             else
+            //                 res.sendStatus(200);
+            //     }
+        );
     }
-        // ,    (error, info) => {
-        //         if (res)
-        //             if (error)
-        //                 res.sendStatus(500);
-        //             else
-        //                 res.sendStatus(200);
-        //     }
-    );
+    catch (exc) {
+        console.error(exc);
+    }
 }
 
 export function tempSetCode(email: string, code: string) {
