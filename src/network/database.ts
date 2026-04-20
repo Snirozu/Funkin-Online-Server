5861a768c306de5f21b7a6091441f4e1185fd130
Snirozu <72814880+Snirozu@users.noreply.github.com>
2026-04-20 19:27:34 +0200

    quick email fix

--- src/network/database.ts
+++ src/network/database.ts
@@ -1560,7 +1560,7 @@ export async function getPlayerByEmail(email: string) {
             where: {
                 email: {
                     equals: email,
-                    mode: "default"
+                    mode: "insensitive"
                 }
             }
         });
